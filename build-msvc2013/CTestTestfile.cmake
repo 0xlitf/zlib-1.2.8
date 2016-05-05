a5779dfe3ddef4b0a985ec629814522590e8d559
Calcifer <calciferlorain@gmail.com>
2016-05-05 16:46:41 +0800

    first commit

---
+++ build-msvc2013/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/_GitHub/zlib-1.2.8
+# Build directory: C:/_GitHub/zlib-1.2.8/build-msvc2013
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
